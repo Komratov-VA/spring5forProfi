3cf70d3c125e6f83c3b14c2764a4db230cfd52bb
komratov <17395719>
2020-05-09 16:47:13 +0300

    InitializingBean interface

---
+++ src/main/java/chapter4/init/PersonInitializingBean.java
@@ -0,0 +1,47 @@
+package chapter4.init;
+
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PersonInitializingBean implements InitializingBean {
+
+    private String name;
+    private Integer age = Integer.MIN_VALUE;
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setAge(Integer age){
+        this.age = age;
+    }
+
+
+    public static void main(String[] args) {
+
+        GenericXmlApplicationContext genericXmlApplicationContext = new GenericXmlApplicationContext();
+        genericXmlApplicationContext.load("chapter4.init/PersonContext.xml");
+        genericXmlApplicationContext.refresh();
+//        String[] strings =genericXmlApplicationContext.getBeanFactory().getBeanDefinitionNames();
+//        System.out.println(strings[0]);
+//        genericXmlApplicationContext.refresh();
+//        Person person = (Person) genericXmlApplicationContext.getBean("firstPerson");
+//        System.out.println(person.age);
+//        System.out.println(person.name);
+        genericXmlApplicationContext.close();
+    }
+
+    public void afterPropertiesSet() throws Exception {
+        if(name == null){
+            this.name = "Bboy";
+        }
+        if(age == Integer.MIN_VALUE){
+            throw new IllegalArgumentException("age is too small");
+        }
+    }
+}
+//<dependency>
+//<groupId>org.springframework</groupId>
+//<artifactId>spring-context</artifactId>
+//<version>3.0.2.RELEASE</version>
+//</dependency>
