bee4a627d666831afe9d705e78a15d7b80aaeed6
komratov <17395719>
2020-05-10 16:49:21 +0300

    BeanNameAware

---
+++ src/main/java/chapter4/init/PersonBeanNameAware.java
@@ -0,0 +1,28 @@
+package chapter4.init;
+
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class PersonBeanNameAware implements BeanNameAware {
+
+    private String name;
+
+    public void setBeanName(String s) {
+        this.name = s;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "bean name = " + name;
+    }
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext genericXmlApplicationContext = new GenericXmlApplicationContext();
+        genericXmlApplicationContext.load("chapter4.init/PersonContext.xml");
+        genericXmlApplicationContext.refresh();
+        PersonBeanNameAware personBeanNameAware = (PersonBeanNameAware)genericXmlApplicationContext.getBean("personBeanNameAware");
+        System.out.println(personBeanNameAware.toString());
+    }
+}
