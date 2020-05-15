14798e790c3e4e20335f8d4c80bdb0d50ad56039
komratov <17395719>
2020-05-15 21:38:25 +0300

    Properties

---
+++ src/main/java/chapter4/init/EnvironmentWithPropertySource.java
@@ -0,0 +1,29 @@
+package chapter4.init;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.context.support.GenericXmlApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.core.env.MutablePropertySources;
+
+public class EnvironmentWithPropertySource {
+
+    public static void main(String[] args) {
+        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext();
+        ctx.refresh();
+        ConfigurableEnvironment conf = ctx.getEnvironment();
+        MutablePropertySources propertySources = conf.getPropertySources();
+        Map<String,Object> appMap = new HashMap<String, Object>();
+        appMap.put("user.home","application_home");
+//        propertySources.addFirst(new MapPropertySource("prospring5_MAP",appMap));
+        propertySources.addLast(new MapPropertySource("prospring5_MAP",appMap));
+        System.out.println("user.home"+ System.getProperty("user.home"));
+        System.out.println("JAVA_HOME"+ System.getenv("JAVA_HOME"));
+        System.out.println("user.home"+ conf.getProperty("user.home"));
+        ctx.close();
+        System.out.println("application_home"+ conf.getProperty("application_home"));
+
+    }
+
+}
